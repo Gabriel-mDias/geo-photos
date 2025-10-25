9737d32d3824a7fd50624570ef069a72b7bba6c4
Gabriel Magalhães <gabrielmagdias@gmail.com>
2025-10-25 19:02:58 -0300

    Bootstrap Icons e ajuste no main-component

--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ViewMapLocalidadesComponent } from './component/view-map-localidades/view-map-localidades.component';
 import { LocalidadesViewComponent } from './localidade/localidades-view/localidades-view.component';
 import { MapRoutingModule } from './map.routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -17,6 +18,7 @@ import { MapRoutingModule } from './map.routing.module';
   imports: [
     CommonModule,
     MapRoutingModule,
+    FontAwesomeModule,
   ],
 })
 export class MapModule { }
